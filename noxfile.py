ad41ce1f7898f84bde1abd8f11339c5ca90be623
bruno <martin.bruno.mail@gmail.com>
2023-04-02 17:54:28 +0200

    2nd attempt to do ci

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,8 @@
 import nox
 
+
 # An example nox task definition that runs on many supported Python versions:
-@nox.session(python=["3.7", "3.8", "3.9"])
+@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
 def test(session):
-    session.run("pytest")
+    session.run("pytest", "--cov=onionizer", "--cov-report=html")
+    session.run("mutmut", "run")
